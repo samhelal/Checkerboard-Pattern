761dd3d853ac134df4cb3815b142786ebeb34345
samhelal <93845103+samhelal@users.noreply.github.com>
2021-11-12 03:54:06 +0200

    Add files via upload

---
+++ Checkerboard-Pattern.cpp
@@ -0,0 +1,21 @@
+/*(Checkerboard Pattern) Display the following
+checkerboard pattern with eight output statements, then display
+the same pattern using as few statements as possible.(c++ how to program)*/
+
+#include <iostream>
+using namespace std ;
+
+int main () {
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+cout << "  * * * * * * * *" <<endl ;
+cout << "* * * * * * * *  " << endl ;
+}
